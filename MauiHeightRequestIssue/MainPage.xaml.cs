e2ee21d9d551386b3eeb7cdf70cdda69f0917ae6
Riccardo11 <minato.riccardo@gmail.com>
2022-12-01 12:43:37 +0100

    Initial commit

---
+++ MauiHeightRequestIssue/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiHeightRequestIssue;
+
+/*
+ * TO TEST THE WORKAROUND
+ * - Uncomment the PropertyChanged event
+ * - Remove from the xaml this line: HeightRequest="{Binding Source={RelativeSource Self}, Path=Width}"
+ */
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+        // ***** UNCOMMENT THIS TO TEST THE WORKAROUND ******
+        // Circle.PropertyChanged += Circle_PropertyChanged;
+	}
+
+    // ***** UNCOMMENT THIS TO TEST THE WORKAROUND ******
+    //private async void Circle_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    //{
+    //    if (e.PropertyName == "Width")
+    //    {
+    //        await Task.Delay(1);  // this is necessary!!
+    //        Circle.HeightRequest = Circle.Width;
+    //    }
+    //}
+}
+
---
+++ MauiHeightRequestIssue/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiHeightRequestIssue;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
